059f00b1aeaf3963f2da2ef252fba294295e5f86
AbhishekB <abhibanerjee2494@gmail.com>
2020-03-12 00:27:35 -0400

    Final Code

--- histogram_equalizer.py
+++ histogram_equalizer.py
@@ -9,18 +9,12 @@ from detection_pipeline import curveFit
 
 
 def adjust_gamma(image, gamma=1.0):
-    # build a lookup table mapping the pixel values [0, 255] to
-    # their adjusted gamma values
     invGamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** invGamma) * 255
         for i in np.arange(0, 256)]).astype("uint8")
-    # apply gamma correction using the lookup table
     return cv2.LUT(image, table)
 
 def hist_equalize(img):
-
-    # img2 = cv2.GaussianBlur(img, (5,5), 0)
-    # img2 = adjust_gamma(img2, 10)
     hist,bins = np.histogram(img.flatten(),256,[0,256])
     cdf = hist.cumsum()
     cdf_m = np.ma.masked_equal(cdf,0)
@@ -28,75 +22,47 @@ def hist_equalize(img):
     cdf_m = cdf_m*255/N
     cdf = np.ma.filled(cdf_m,0).astype('uint8')
     img = cdf[img]
-    # cv2.imshow("image", img2)
-    # cv2.waitKey(0)
-    # plt.imshow(img2)
-    # plt.show()
+
     return img
 
 def hough_tf(img, img_):
 
-
-    # img_ = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   
-    # cv2.imshow("test", img_)
-    # cv2.waitKey(0)
-    # img_ = cv2.equalizeHist(img_)
-    # cv2.imshow("test", img_)
-    # cv2.waitKey(0)
-    # return [], [], img_       
-    # img_ = adjust_gamma(img_, 2)
     lower_color_bound = (0, 150, 180)
     upper_color_bound = (200, 180, 190) 
-    # _, img_ = cv2.threshold(img_, 200, 255, cv2.THRESH_BINARY)
+
     img_thresh = cv2.inRange(img, lower_color_bound, upper_color_bound) ## Does a yellow filter and keeps the white lanes as well. 
-    # cv2.imshow("a", img_)
-    # cv2.waitKey(0)
+
 
     edges = cv2.Canny(img_thresh, 20, 20, apertureSize=3) ## finds lines on a thresholded image using canny. output is edges in random directions
-    # cv2.imshow("a", edges)
-    # cv2.waitKey(0)
+
     edges = cv2.GaussianBlur(edges,(35,35),0) # Blurred the random direction edges to no longer see them as edges but as spots on lanes 
-    # cv2.imshow("a", edges)
-    # cv2.waitKey(0)
+
     
-    # print(np.median(img))
+
     if np.median(img_) < 100: # when entering the shaded area, enhance the edges and then do the operation on the image
 
         img_sharped = cv2.addWeighted(img, 8, cv2.blur(img, (35, 35)), -8, 128) # sharpen
-        # cv2.imshow("median", img_sharped)
-        # cv2.waitKey(0)
+
         lower_color_bound = (140, 150, 135) ## use a filter to find points on the lanes
         upper_color_bound = (200, 250, 220) ## use a filter to find points on the lanes , this was very tricky and a narrow range. 
         img_thresh = cv2.inRange(img_sharped, lower_color_bound, upper_color_bound) # thresholding 
         
         edges = cv2.Canny(img_thresh, 20, 20, apertureSize=3)
-        # cv2.imshow("a", edges)
-        # cv2.waitKey(0)
+
         edges = cv2.GaussianBlur(edges,(35,35),0)
     
     elif np.median(img) > 158: # when exiting the shaded area, 
 
-        # img_sharped = cv2.addWeighted(img, 4, cv2.blur(img, (35, 35)), -4, 128) # sharpen
-        # cv2.imshow("median", img_sharped)
-        # cv2.waitKey(0)
         lower_color_bound = (120, 180, 200) ## use a filter to find points on the lanes
         upper_color_bound = (200, 250, 250) ## use a filter to find points on the lanes , this was very tricky and a narrow range. 
         img_thresh = cv2.inRange(img, lower_color_bound, upper_color_bound) # thresholding 
         
         edges = cv2.Canny(img_thresh, 20, 20, apertureSize=3)
-        # cv2.imshow("a", edges)
-        # cv2.waitKey(0)
         edges = cv2.GaussianBlur(edges,(35,35),0)
 
-        # cv2.imshow("a", edges)
-        # cv2.waitKey(0)
 
-
-    # cv2.imshow(" s", edges)
-    # cv2.waitKey(0)
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, 10, 10)
-    #left_pts = np.array([])
-    #right_pts = np.array([])
+
     left_pts = []
     right_pts = []
     for x1, y1, x2, y2, in lines[:,0,:]:
@@ -108,16 +74,15 @@ def hough_tf(img, img_):
             continue
 
         if x1 < img.shape[1]/2:
-            #left_pts = np.append(left_pts, [x1, y1])
+
             left_pts.append(([x1,y1]))
         elif x1 > img.shape[1]/2:
-            #right_pts = np.append(right_pts, [x1, y1])
+
             right_pts.append(([x1,y1]))     
         if x2 < img.shape[1]/2:
-            #left_pts = np.append(left_pts, [x2, y2])
+
             left_pts.append(([x1,y1]))
         elif x2 > img.shape[1]/2:
-            #right_pts = np.append(right_pts, [x2, y2])
             right_pts.append(([x1,y1]))
                          
         cv2.line(img, (x1,y1), (x2, y2), (0,255,0), 2)
@@ -137,56 +102,29 @@ def hough_tf(img, img_):
             right_pts = np.zeros_like(left_pts)
         
     cv2.imshow(" s", img)
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
     return left_pts, right_pts, img
 
 
 
 def hough_tf_image(img, img_):
 
-    
-    # img_ = hist_equalize(img_)
-    # img_ = adjust_gamma(img_, 2)
-    # img_ = cv2.GaussianBlur(img_,(5,5),0)
     _, img_thresh = cv2.threshold(img_, 220, 255, cv2.THRESH_BINARY) # thresholding image to get a high intensity
-    # cv2.imshow("fig", img_thresh)
-    # cv2.waitKey(0)
-    # cv2.imshow("edges1", img_thresh)
-    # cv2.waitKey(0)
 
-    # REMOVED CANNY FOR NON SHADDY REGIONS TO GET MORE HOUGH LINES
-    # edges = cv2.Canny(img_thresh, 20, 20, apertureSize=3) # find edges on thresholded images
-    # cv2.imshow("edges1", edges)
-    # cv2.waitKey(0)
+
     edges = cv2.GaussianBlur(img_thresh,(49,49),0) # Blur the edges to so that random direction lines are just seen as edge patches rather than edges
-    # cv2.imshow("edges1", edges)
-    # cv2.waitKey(0)
+
 
     if np.median(img_) < 100:
-        # img_sharped = cv2.addWeighted(img_, 4, cv2.blur(img_, (35, 35)), -4, 128)
-        # cv2.imshow("edges1", img_)
-        # cv2.waitKey(0)
-        # img_sharped = hist_equalize(img_) # Equalise the image when image is dull
-        # cv2.imshow("edges1", img_sharped)
-        # cv2.waitKey(0)
+
         _, img_thresh2 = cv2.threshold(img_, 80, 255, cv2.THRESH_BINARY) # threshold the histogram equalised img
-        # cv2.imshow("edges1", img_thresh2)
-        # cv2.waitKey(0)
+
         edges2 = cv2.Canny(img_thresh2, 3, 3, apertureSize=3) # 
-        # edges2 = cv2.addWeighted(edges2, 4, cv2.blur(edges2, (35, 35)), -4, 128)
+
         edges2 = cv2.GaussianBlur(edges2,(5,5),0)
-        # cv2.imshow("edges1", edges2)
-        # cv2.waitKey(0)
-        # cv2.imshow("edges2", edges2)
-        # cv2.waitKey(0)
+
         edges = edges + edges2
         edges = adjust_gamma(edges, 1.4)
-        # cv2.imshow("edges1", edges)
-        # cv2.waitKey(0)
-        # print(np.max(edges))
-    # edges = adjust_gamma(edges, 2)
-    # cv2.imshow(" s", edges)
-    # cv2.waitKey(0)
 
     lines = cv2.HoughLinesP(edges, 1, np.pi/180, 30, 10, 10)
     left_pts = []
@@ -215,40 +153,9 @@ def hough_tf_image(img, img_):
     elif right_pts.shape == (0,):
         right_pts = np.zeros_like(left_pts)
     cv2.imshow(" s", img)
-    cv2.waitKey(0)
-    # print(right_pts.shape)
     return left_pts, right_pts, img 
 
 def hist_count(img, img_):
-
-    # img = cv2.medianBlur(img, 81)
-    # img = cv2.GaussianBlur(img,(5,5),0)
-    # img_sobel = cv2.Sobel(img, cv2.CV_64F, 1, 0, 5)
-    # img_sobel[..., 0] = ((img_sobel[..., 0] - np.min(img_sobel[..., 0]))*255/(np.max(img_sobel[..., 0]))).astype(np.uint8)
-    # img_sobel[..., 1] = ((img_sobel[..., 1] - np.min(img_sobel[..., 1]))*255/(np.max(img_sobel[..., 1]))).astype(np.uint8)
-    # img_sobel[..., 2] = ((img_sobel[..., 2] - np.min(img_sobel[..., 2]))*255/(np.max(img_sobel[..., 2]))).astype(np.uint8)
-    # print(np.min(img_sobel))
-    # print(img_sobel)
-    # exit(-1)
-    # cv2.imshow("sobel", img)
-    # cv2.waitKey(0)
-    
-    # hist0,bins0 = np.histogram(img[:,:,0].flatten(),256,[0,256])
-    # hist1,bins1 = np.histogram(img[:,:,1].flatten(),256,[0,256])
-    # hist2,bins2 = np.histogram(img[:,:,2].flatten(),256,[0,256])
-    # plt.plot(bins0[0:256], hist0, 'b')
-    # plt.plot(bins1[0:256], hist1, 'g')
-    # plt.plot(bins2[0:256], hist2, 'r')
-    # plt.show()
-    # plt.close()
-    # lower_color_bound = (0, 150, 150) ## use a filter to find points on the lanes
-    # upper_color_bound = (140, 195, 195) ## use a filter to find points on the lanes , this was very tricky and a narrow range.
-    # img_thresh = cv2.inRange(img, lower_color_bound, upper_color_bound) # thresholding
-    # cv2.imshow("yellow hopefully", img_thresh) 
-    # cv2.waitKey(0)
-    # exit(-1)
-
-    # for x, y do blue image plot. 
     fig1 = plt.figure()
     ax1 = fig1.gca(projection='3d')
     graph = np.zeros([img.shape[0], img.shape[1], 2])
@@ -259,7 +166,6 @@ def hist_count(img, img_):
 
     X, Y = np.meshgrid(X, Y)
     Z = (img[X, Y,1] + img[X,Y,2])/(abs(img[X,Y,1] - img[X,Y,2]) + 1)
-    # print(np.arange(0,graph.shape[0]).shape, np.arange(0,graph.shape[1]).shape, graph[:,:,1].shape)
     surf = ax1.plot_surface(X, Y, Z)
     fig2 = plt.figure()
     ax2 = fig2.gca(projection='3d')
@@ -267,11 +173,6 @@ def hist_count(img, img_):
     surf = ax2.plot_surface(X, Y, Z2)
     plt.show()
     exit(-1)
-    # surf = (X, Y, Z, cmap=cm.coolwarm,
-                    #    linewidth=0, antialiased=False)
-    # hist_0 = cv2.calcHist([img],[0],None,[256],[0,256])
-    # hist_1 = cv2.calcHist([img],[1],None,[256],[0,256])
-    # hist_2 = cv2.calcHist([img],[2],None,[256],[0,256])
 
     cv2.imshow("lines",img)
     #cv2.waitKey(0)
@@ -322,39 +223,32 @@ if __name__ == '__main__':
             righty = arr_tempr[arr_tempr.shape[0]-1]  
             flag =1  
             lines,prediction = curveFit(lefty,righty,lines,flag)
-            #print(prediction)
             Hinv = np.linalg.inv(H)
             Hinv = Hinv/Hinv[2,2]
             blank_white = 255*np.ones([720, 1280, 3])
-            # blank_black = 255*np.zeros([720, 1280, 3])
             blank_backProj = cv2.warpPerspective(blank_white, Hinv, (1280, 720))
             _, thresh1 = cv2.threshold(blank_backProj, 127, 255, cv2.THRESH_BINARY_INV)
-            # cv2.imshow("test", thresh1)
-            # cv2.waitKey(0)
+
             patch = np.logical_and(img, thresh1)
             img_dummy = np.zeros_like(img)
             img_dummy[patch] = img[patch]
             backProjected = cv2.warpPerspective(lines, Hinv, (1280, 720))
-            # patch2 = np.logical_or(img_dummy, backProjected)
+
             img_dummy = img_dummy + backProjected
-            #prediction = predicTurn(img_dummy,L,R)
+
             cv2.putText(img_dummy, prediction, (200, 50),cv2.FONT_HERSHEY_SIMPLEX, 1.5,(0,255,0),2,\
             cv2.LINE_AA)
-            # print(backProjected)
             cv2.imshow("test", img_dummy)
-            #cv2.imwrite("lane_mask_pred.jpg",img_dummy)
-            cv2.waitKey(0)
 
             vidWriter.write(img_dummy.astype(np.uint8))
     
     elif sys.argv[2] == 'png':
 
         files = np.sort(glob.glob(sys.argv[1] + '/*', recursive=True))
-        vidWriter = cv2.VideoWriter("video_output.mp4",cv2.VideoWriter_fourcc(*'mp4v'), 24, (1280,720))
+        vidWriter = cv2.VideoWriter("video_output.mp4",cv2.VideoWriter_fourcc(*'mp4v'), 24, (1392,512))
         arrl_store = []
         arrr_store = []
         for file in files:
-            # print(file)
             img = cv2.imread(file)
 
             src_pt = np.array([[526, 322], [767, 328],[354, 428],[866, 434] ])
@@ -379,11 +273,9 @@ if __name__ == '__main__':
             righty = arr_tempr[arr_tempr.shape[0]-1]
             flag = 0    
             lines,prediction = curveFit(lefty,righty,lines,flag)
-            # hist_count(warped, warped_)
             Hinv = np.linalg.inv(H)
             Hinv = Hinv/Hinv[2,2]
             blank_white = 255*np.ones([img.shape[0], img.shape[1], 3])
-            # blank_black = 255*np.zeros([720, 1280, 3])
             blank_backProj = cv2.warpPerspective(blank_white, Hinv, (img.shape[1], img.shape[0]))
             _, thresh1 = cv2.threshold(blank_backProj, 127, 255, cv2.THRESH_BINARY_INV)
             patch = np.logical_and(img, thresh1)
@@ -394,11 +286,7 @@ if __name__ == '__main__':
             cv2.putText(img_dummy, prediction, (200, 50),cv2.FONT_HERSHEY_SIMPLEX, 1.5,(0,255,0),2,\
             cv2.LINE_AA)
 
-            cv2.imshow("test", img_dummy)
-            #cv2.imwrite("lane_mask_stat.jpg",img_dummy)
-            cv2.waitKey(0)
             vidWriter.write(img_dummy.astype(np.uint8))
-    vidWriter.release()
 
     
     vidWriter.release()
