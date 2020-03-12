6693ba5066be6789e1d3b3779c08a473d467efe3
AbhishekB <abhibanerjee2494@gmail.com>
2020-03-12 00:26:47 -0400

    Final Code

--- detection_pipeline.py
+++ detection_pipeline.py
@@ -11,21 +11,9 @@ def undistortImage(image):
 	img = cv2.GaussianBlur(img,(3,3),0)
 	return img
 
-def predicTurn(img,l_point,r_point):
-    i_center = img.shape[1]/2-25
-    m_point = l_point + (r_point-l_point)/2
-    print(m_point,i_center)
-    if abs(m_point-i_center)<15:
-        predict = "Straight"
-    elif (m_point-i_center)<-30:
-        predict = "Left Turn"
-    else:
-        predict = "Right Turn"
-    return predict
 
 def radiusCurvatureStat(p_x,p_y):
 	radC = ((1 + (2*p_y[0]*p_x + p_y[1])**2)**(1.5)) / (2*p_y[0])
-	print(np.mean(radC))
 	crad = np.mean(radC)
 	if crad>8000:
 		pred = "Turn Right"
@@ -38,7 +26,6 @@ def radiusCurvatureStat(p_x,p_y):
 
 def radiusCurvature(p_x,p_y):
 	radC = ((1 + (2*p_y[0]*p_x + p_y[1])**2)**(1.5)) / (2*p_y[0])
-	# print(np.mean(radC))
 	crad = np.mean(radC)
 	if crad>3000:
 		pred = "Turn Right"
@@ -58,32 +45,28 @@ def curveFit(left,right,img,flag):
 	wspace = np.linspace(240,img.shape[0]-1, img.shape[0]-240)
 	left_fit = left_poly(wspace)
 	right_fit = right_poly(wspace)
-	#coordinates_left = np.hstack([left_fit,wspace])
 	coordinates_left = np.array([np.transpose(np.vstack([left_fit, wspace]))])
 	coordinates_left = coordinates_left[0].astype(np.int32)
-	#coordinates_right = np.hstack([right_fit,wspace])
 	coordinates_right = np.array([np.transpose(np.vstack([right_fit, wspace]))])
 	coordinates_right = coordinates_right[0].astype(np.int32)
 	cv2.polylines(img, [coordinates_left], False, (0,0,0),10)
 	cv2.polylines(img, [coordinates_right], False, (0,0,0),10)
 	lPoint, rPoint = coordinates_left[200][0], coordinates_right[200][0]
-	#lPoint, rPoint = coordinates_left[0][0], coordinates_left[150][0]
-	#pred = predicTurn(img,lPoint,rPoint)
 	if flag==0:
 		pred = radiusCurvatureStat(wspace,right_lane)
 	else:
 		pred = radiusCurvature(wspace,right_lane)
 	points = np.hstack((coordinates_left, coordinates_right))
 	points = np.array([points],dtype=np.int32)[0]
-	print(points.shape)
-	#points = np.array([[[points[0,0],points[0,1]],[points[0,2],points[0,3]],[points[points.shape[0]/2,2]\
-	#	,points[points.shape[0]/2,3]]\
-	#	,[points[points.shape[0]-1,2],points[points.shape[0]-1,3]]\
-	#	,[points[points.shape[0]-1,0],points[points.shape[0]-1,1]],[points[points.shape[0]/2,0],points[points.shape[0]/2,1]]]])
-	points = np.array([[[points[0,0],points[0,1]],[points[0,2],points[0,3]],\
-		[points[points.shape[0]-1,2],points[points.shape[0]-1,3]],[points[points.shape[0]-1,0],points[points.shape[0]-1,1]]]])
+	if flag==1:
+		points = np.array([[[points[0,0],points[0,1]],[points[0,2],points[0,3]],[points[points.shape[0]/2,2]\
+			,points[points.shape[0]/2,3]]\
+			,[points[points.shape[0]-1,2],points[points.shape[0]-1,3]]\
+			,[points[points.shape[0]-1,0],points[points.shape[0]-1,1]],[points[points.shape[0]/2,0],points[points.shape[0]/2,1]]]])
+	else:
+		points = np.array([[[points[0,0],points[0,1]],[points[0,2],points[0,3]],\
+			[points[points.shape[0]-1,2],points[points.shape[0]-1,3]],[points[points.shape[0]-1,0],points[points.shape[0]-1,1]]]])
 	cv2.fillPoly(mask,points,(0,255,0))
-	#image = cv2.warpPerspective(mask, np.linalg.inv(H),(IMG.shape[1], IMG.shape[0]))
 	fimage = cv2.addWeighted(img, 0.5, mask, 0.5, 0.0)
 
 	return fimage, pred
@@ -91,12 +74,11 @@ def curveFit(left,right,img,flag):
 def main():
 	fname ='../Problem_2/data_1/data/0000000015.png'
 	IMG = cv2.imread(fname)
-	# print(IMG.shape)
+
 	IMG = IMG[230:IMG.shape[0],:,:]
 	IMG = cv2.cvtColor(IMG, cv2.COLOR_BGR2GRAY)
 	udimg = undistortImage(IMG)
 	cv2.imshow("Undistorted",udimg)
-	#cv2.imshow("Distorted",IMG)
 	cv2.waitKey(0)
 
 
